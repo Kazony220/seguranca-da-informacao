e4a2cb43cf6746964242b302c8419bc7a636b6e5
Kazony220 <luizotaviococ2@gmail.com>
2025-02-25 14:38:31 +0000

    Atualiza o script do database

--- aula01/criacao.sql
+++ aula01/criacao.sql
@@ -1,6 +1,13 @@
+-- criação barra de dados
 create or replace database fatec;
-
+--seleciona a barra de dados ativa
 use fatec;
+-- or replace apaga tudo
+create or replace table produtos(
+    codigo smallint unsigned,
+    nome varchar(100),
+    quantidade tinyint 
 
-show databases;
+
+);
 
