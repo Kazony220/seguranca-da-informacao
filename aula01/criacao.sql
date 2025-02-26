931dd88df6788b1ec100e637e2c3444c7d31a654
Kazony220 <luizotaviococ2@gmail.com>
2025-02-26 20:42:06 +0000

    5 tabelas

--- aula01/criacao.sql
+++ aula01/criacao.sql
@@ -1,13 +1,28 @@
 -- criação barra de dados
-create or replace database fatec;
---seleciona a barra de dados ativa
-use fatec;
--- or replace apaga tudo
-create or replace table produtos(
-    codigo smallint unsigned,
-    nome varchar(100),
-    quantidade tinyint 
-
+DROP DATABASE IF EXISTS fatec;
+CREATE DATABASE fatec;
+USE fatec;
+CREATE TABLE produtos (
+    codigo SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    quantidade TINYINT NOT NULL,
+    preco DECIMAL(6, 2) UNSIGNED NOT NULL,
+    validade DATE,
+    ativo BOOLEAN NOT NULL,
+    PRIMARY KEY (codigo),
+    UNIQUE KEY (nome)
 
 );
+
+CREATE TABLE clientes (
+    id_cliente INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20),
+    data_cadastro DATE NOT NULL,
+    ativo BOOLEAN NOT NULL,
+    PRIMARY KEY (id_cliente),
+    UNIQUE KEY (email)
+);
+
 
